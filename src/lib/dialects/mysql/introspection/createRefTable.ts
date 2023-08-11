1d2ee767817237ca77b911a22df9b189f80bec48
nickapplicable <numbereft@gmail.com>
2023-08-12 02:57:21 +1200

    make ref table use varchar instead of uuid for id

--- src/lib/dialects/mysql/introspection/createRefTable.ts
+++ src/lib/dialects/mysql/introspection/createRefTable.ts
@@ -3,7 +3,7 @@ import { QueryFn } from '../../../types'
 export const createRefTable = async (query: QueryFn) => {
 	await query(`
 		CREATE TABLE IF NOT EXISTS _ref (
-			id uuid not null primary key,
+			id varchar(36) not null primary key,
 			type text not null,
 			name text not null,
 			tableName text null
